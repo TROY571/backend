41b4b7198989dd316065e7d996c949bc1ee7136c
Siyu <1021109492@qq.com>
2024-08-05 20:02:13 +0200

    remove unused lines

--- src/main/java/com/example/backend/config/WebConfig.java
+++ src/main/java/com/example/backend/config/WebConfig.java
@@ -4,7 +4,6 @@ import com.example.backend.interceptors.JWTInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -17,18 +16,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
                 .allowCredentials(true);
     }
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/videos/**","/files/**")
-//                .addResourceLocations("file:/home/uploadPath/upload/");
-//    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new JWTInterceptor())
                 .excludePathPatterns("/api/users/login")
                 .excludePathPatterns("/api/users/img/**")
-                .excludePathPatterns("/api/videos/files/**")
-        ;
-
+                .excludePathPatterns("/api/videos/files/**");
     }
 }
